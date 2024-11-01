c280bdb4bad2113648f54bfcc748c2bf34486051
DajaHraje <38384702+DajaHraje@users.noreply.github.com>
2024-11-01 11:42:57 +0100

    Update Program.cs
    dodělal jsem random k policajtovi když se zmáčkne W. šance potkání policajta na cestě je 1/15

--- idk/idk/Program.cs
+++ idk/idk/Program.cs
@@ -1,18 +1,18 @@
 Console.WriteLine("Chceš jít na PC nebo XBOX servery.");
-string server = Console.ReadLine(); 
-    if (server == "XBOX")
-    {
-        Console.WriteLine("Připojil jsi se na XBOX servery.");
-    }
-    else if (server == "PC")
-    {
+string server = Console.ReadLine();
+if (server == "XBOX")
+{
+    Console.WriteLine("Připojil jsi se na XBOX servery.");
+}
+else if (server == "PC")
+{
     Console.WriteLine("jdeš cheatovat ty hajzle že.");
-    }
-    else
-    {
+}
+else
+{
     Console.WriteLine("Na take servery nemame budget kamo");
     Console.WriteLine("Tvůj progress se nebude ukládat (nevim jak ti to mam vypnout tak proto)");
-    }
+}
 
 bool opakovani = true;
 
@@ -22,24 +22,34 @@ while (opakovani == true)
     string vyber = Console.ReadLine();
     if (vyber == "W")
     {
-        Console.WriteLine("Šel jsi dopředu");
+        Random random = new Random();
+        int nahodnePolda = random.Next(0, 15);
+        if (nahodnePolda == 6)
+        {
         Console.WriteLine("Našel si poldu, co uděláš? chceš ho zabít [A.N]");
         string vyberPolda = Console.ReadLine();
         if (vyberPolda == "A")
         {
             Console.WriteLine("Zabil si poldu");
-        } else if (vyberPolda == "N")
+        }
+        else if (vyberPolda == "N")
         {
             Console.WriteLine("Nezabil si poldu");
-        } else
+        }
+        else
         {
             Console.WriteLine("Polda tě zatkl");
             Console.ForegroundColor = ConsoleColor.Blue;
         }
-    } else if (vyber == "A")
+    } else
+        {
+        Console.WriteLine("Šel jsi dopředu");
+        }
+    }
+    else if (vyber == "A")
     {
         Random random = new Random();
-        int nahodneBanka = random.Next(0, 21); 
+        int nahodneBanka = random.Next(0, 21);
         if (nahodneBanka == 18)
         {
             Console.WriteLine("Udělal si banku");
@@ -52,7 +62,7 @@ while (opakovani == true)
                     break;
                 case "B":
                     Console.WriteLine("Bojoval si, ale umřel si");
-                    break ;
+                    break;
                 case "C":
                     Console.WriteLine("To nejde, ale chytli tě");
                     break;
@@ -60,30 +70,37 @@ while (opakovani == true)
                     Console.WriteLine("Umřel si na dehydrataci");
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("WASTED");
+                    Console.ResetColor();
                     break;
             }
-            
+
         }
         else
         {
             Console.WriteLine("Šel jsi doleva");
         }
-    } else if (vyber == "S")
+    }
+    else if (vyber == "S")
     {
         Console.WriteLine("Šel jsi dozadu");
-    } else if (vyber == "D")
+    }
+    else if (vyber == "D")
     {
         Console.WriteLine("Šel jsi doprava");
     }
     else if (vyber == "04122007")
     {
-            Console.WriteLine("Našel jsi secret a zabijíš Trevora");
-            Console.WriteLine("Victory Royale");
-        } else
+        Console.WriteLine("Našel jsi secret a zabijíš Trevora");
+        Console.ForegroundColor= ConsoleColor.DarkYellow;
+        Console.WriteLine("Victory Royale");
+        Console.ResetColor();
+    }
+    else
     {
         Console.WriteLine("Trevor tě zabil");
         Console.ForegroundColor = ConsoleColor.DarkRed;
         Console.WriteLine("Wasted");
+        Console.ResetColor();
         opakovani = false;
     }
 }
