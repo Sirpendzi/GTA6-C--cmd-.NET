747f22748e018b75510298ffba3d3849d1fef6b2
DajaHraje <38384702+DajaHraje@users.noreply.github.com>
2024-11-01 16:43:44 +0100

    Update Program.cs
    dodělávání chop heistu, předělání všech Console.ReadLine(); na To.Lower(); , Pokračovat budu v neděli, funkční, ale nedokončená verze...
    
    Prosím, jestli do toho budete někdo psát, napiště do commit changes, co jste změnili ať vím v čem je kdyžtak chyba.
    
    Piště klidně nápady na GTA 6 teams a klidně tam něco dopíšu.
    
    
    luv, david <3

--- idk/idk/Program.cs
+++ idk/idk/Program.cs
@@ -1,10 +1,10 @@
 Console.WriteLine("Chceš jít na PC nebo XBOX servery.");
-string server = Console.ReadLine();
-if (server == "XBOX")
+string server = Console.ReadLine().ToLower();
+if (server == "xbox")
 {
     Console.WriteLine("Připojil jsi se na XBOX servery.");
 }
-else if (server == "PC")
+else if (server == "pc")
 {
     Console.WriteLine("jdeš cheatovat ty hajzle že.");
 }
@@ -19,34 +19,35 @@ bool opakovani = true;
 while (opakovani == true)
 {
     Console.WriteLine("Kam chceš jít [W,A,S,D]");
-    string vyber = Console.ReadLine();
-    if (vyber == "W")
+    string vyber = Console.ReadLine().ToLower();
+    if (vyber == "w")
     {
         Random random = new Random();
         int nahodnePolda = random.Next(0, 7);
         if (nahodnePolda == 4)
         {
-        Console.WriteLine("Našel si poldu, co uděláš? chceš ho zabít [A.N]");
-        string vyberPolda = Console.ReadLine();
-        if (vyberPolda == "A")
-        {
-            Console.WriteLine("Zabil si poldu");
-        }
-        else if (vyberPolda == "N")
-        {
-            Console.WriteLine("Nezabil si poldu");
+            Console.WriteLine("Našel si poldu, co uděláš? chceš ho zabít [ANO / NE]");
+            string vyberPolda = Console.ReadLine().ToLower();
+            if (vyberPolda == "ano")
+            {
+                Console.WriteLine("Zabil si poldu");
+            }
+            else if (vyberPolda == "ne")
+            {
+                Console.WriteLine("Nezabil si poldu");
+            }
+            else
+            {
+                Console.WriteLine("Polda tě zatkl");
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
         }
         else
         {
-            Console.WriteLine("Polda tě zatkl");
-            Console.ForegroundColor = ConsoleColor.Blue;
-        }
-    } else
-        {
-        Console.WriteLine("Šel jsi dopředu");
+            Console.WriteLine("Šel jsi dopředu");
         }
     }
-    else if (vyber == "A")
+    else if (vyber == "a")
     {
         Random random = new Random();
         int nahodneBanka = random.Next(0, 21);
@@ -54,16 +55,16 @@ while (opakovani == true)
         {
             Console.WriteLine("Udělal si banku");
             Console.WriteLine("Co ted? A pro utek, B pro boj, C pro sebevraždu");
-            string volbaBanka = Console.ReadLine();
+            string volbaBanka = Console.ReadLine().ToLower();
             switch (volbaBanka)
             {
-                case "A":
+                case "a":
                     Console.WriteLine("Utekl si");
                     break;
-                case "B":
+                case "b":
                     Console.WriteLine("Bojoval si, ale umřel si");
                     break;
-                case "C":
+                case "c":
                     Console.WriteLine("To nejde, ale chytli tě");
                     break;
                 default:
@@ -80,7 +81,7 @@ while (opakovani == true)
             Console.WriteLine("Šel jsi doleva");
         }
     }
-    else if (vyber == "S")
+    else if (vyber == "s")
     {
         Random random = new Random();
         int krokDozadu = random.Next(0, 40);
@@ -97,6 +98,149 @@ while (opakovani == true)
                     switch (volbaPracicky)
                     {
                         case "chop":
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write("Lamar: ");
+                            Console.ResetColor ();
+                            Console.WriteLine("„Hele, kámo, potřebuju tvou pomoc. Můj pes, Chop, se dostal do průšvihu, brácho. Nějaký divný týpci ho unesli.");
+                            Console.WriteLine("Říkají, že jestli se do toho vložíme, nebude to pěkný... Potřebuju někoho, kdo se nebojí, chápeš? Jsi ready? [ANO / NE]“");
+                            string volbaChop1 = Console.ReadLine().ToLower();
+                            bool opakovaniChop1 = true;
+                            while (opakovaniChop1 == true)
+                            {
+                                switch (volbaChop1)
+                                {
+                                case "ano":
+                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                    Console.Write("Lamar: ");
+                                    Console.ResetColor();
+                                    Console.WriteLine("„Dobrej kluk, věděl jsem, že na tebe se můžu spolehnout! Tady je plán – začneme tím, že prozkoumáme jejich doupě v South Los Santos.");
+                                    Console.WriteLine("Slyšel jsem, že ti týpci kolem tohohle místa často krouží. Jdeme na to, jo?“ [ANO / NE]");
+                                    string volbaChop2 = Console.ReadLine().ToLower();
+                                    if (volbaChop2 == "ano")
+                                    {
+                                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                                            Console.Write("Lamar: ");
+                                            Console.ResetColor();
+                                            Console.WriteLine("„Fajn, počítal jsem s tebou! Počítej s tím, že to nebude procházka růžovým sadem. Ti týpci jsou ostří.");
+                                            Console.WriteLine("Máme tři možnosti, jak na to jít. Můžeme to vzít opatrně a vplížit se zezadu.");
+                                            Console.WriteLine("Nebo to můžeme vzít přímo – ale to už bude pořádná akce");
+                                            Console.WriteLine("Taky můžeme zkusit odlákat pozornost a pak se k Chopovi dostat.“ [1 - Opatrný přístup (plížení) / 2 - Přímý útok (akce) / 3 - Odlákat pozornost (past)]");
+                                            int volbaChop4 = Convert.ToInt32 (Console.ReadLine());
+                                            switch (volbaChop4)
+                                            {
+                                                case 1:
+                                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                    Console.Write("Lamar: ");
+                                                    Console.ResetColor();
+                                                    Console.WriteLine("„Dobře, plížíme se! Ticho a klidně, jinak si nás všimnou. Vidím tam dva týpky.");
+                                                    Console.WriteLine("Buď se můžeme připlížit a vyřadit je potichu, nebo je obejdeme. Co si myslíš?“");
+                                                    Console.WriteLine("1 - Zabít je / 2 - Obejít je");
+                                                    // POKRAČOVAT
+                                                    break;
+                                                case 2:
+                                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                    Console.Write("Lamar: ");
+                                                    Console.ResetColor();
+                                                    // POKRAČOVAT
+                                                    break;
+                                                case 3:
+                                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                    Console.Write("Lamar: ");
+                                                    Console.ResetColor();
+                                                    // POKRAČOVAT
+                                                    break;
+                                                default:
+                                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                    Console.Write("Lamar: ");
+                                                    Console.ResetColor();
+                                                    Console.WriteLine("To sis u mě posral kámo... To jsem od tebe nečekal.");
+                                                    Console.WriteLine("Už si Lamara nasral a zastřelil tě!");
+                                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                    Console.WriteLine("Wasted");
+                                                    Console.ResetColor();
+                                                    opakovani = false;
+                                                    opakovaniChop1 = false;
+                                                    break;
+                                            }
+                                    } else
+                                    {
+                                        Console.WriteLine("Bohužel kámo, není cesty zpět, už jsi mi to odsouhlasil.");
+                                    }
+                                    break;
+                                case "ne":
+                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                        Console.Write("Lamar: ");
+                                        Console.ResetColor();
+                                        Console.WriteLine("„Cože, fakt? Hele, já chápu, jestli tě to děsí, ale teď nemáme čas váhat. Chop potřebuje pomoc.");
+                                        Console.WriteLine("Jdeš, nebo tě mám někam odložit, dokud to nebude hotový?“ [ANO / NE]");
+                                        string volbaChop3 = Console.ReadLine().ToLower();
+                                        switch (volbaChop3)
+                                        {
+                                            case "ano":
+                                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                Console.Write("Lamar: ");
+                                                Console.ResetColor();
+                                                Console.WriteLine("Kámo, věděl jsem, že se na mě nevykašleš.");
+                                                break;
+                                            case "ne":
+                                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                Console.Write("Lamar: ");
+                                                Console.ResetColor();
+                                                Console.WriteLine("To sis u mě posral kámo... To jsem od tebe nečekal.");
+                                                Console.WriteLine("Už si Lamara nasral a zastřelil tě!");
+                                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                Console.WriteLine("Wasted");
+                                                Console.ResetColor();
+                                                opakovani = false;
+                                                opakovaniChop1 = false;
+                                                break;
+                                            default:
+                                                Console.WriteLine("Je to ano nebo ne? [ANO / NE]");
+                                                string volbaChop4 = Console.ReadLine().ToLower();
+                                                switch(volbaChop4)
+                                                {
+                                                    case "ano":
+                                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                        Console.Write("Lamar: ");
+                                                        Console.ResetColor();
+                                                        Console.WriteLine("Kámo, věděl jsem, že se na mě nevykašleš.");
+                                                        //POKRAČOVÁNÍ VLOŽIT Z ŘÁDKU 121
+                                                        break;
+                                                    case "ne":
+                                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                        Console.Write("Lamar: ");
+                                                        Console.ResetColor();
+                                                        Console.WriteLine("To sis u mě posral kámo... To jsem od tebe nečekal.");
+                                                        Console.WriteLine("Už si Lamara nasral a zastřelil tě!");
+                                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                        Console.WriteLine("Wasted");
+                                                        Console.ResetColor();
+                                                        opakovani = false;
+                                                        opakovaniChop1 = false;
+                                                        break;
+                                                    default:
+                                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                        Console.Write("Lamar: ");
+                                                        Console.ResetColor();
+                                                        Console.WriteLine("To sis u mě posral kámo... To jsem od tebe nečekal.");
+                                                        Console.WriteLine("Už si Lamara nasral a zastřelil tě!");
+                                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                                        Console.WriteLine("Wasted");
+                                                        Console.ResetColor();
+                                                        opakovani = false;
+                                                        opakovaniChop1 = false;
+                                                        break;
+                                                }
+                                                break;
+                                        }
+                                        break;
+                                default:
+                                        Console.WriteLine("Nezvykej si na to, že ti to bude vycházet pokaždé. Jednou ti to Lamar vrátí");
+                                        opakovaniChop1 = false;
+                                    break;
+                                }
+
+                            }
                             break;
                         case "fleeca":
                             break;
@@ -115,20 +259,20 @@ while (opakovani == true)
                     Console.WriteLine("Jsi kokot?");
                     break;
             }
-        } 
+        }
         else
         {
-        Console.WriteLine("Šel jsi dozadu");
+            Console.WriteLine("Šel jsi dozadu");
         }
     }
-    else if (vyber == "D")
+    else if (vyber == "d")
     {
         Console.WriteLine("Šel jsi doprava");
     }
     else if (vyber == "04122007")
     {
         Console.WriteLine("Našel jsi secret a zabijíš Trevora");
-        Console.ForegroundColor= ConsoleColor.DarkYellow;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
         Console.WriteLine("Victory Royale");
         Console.ResetColor();
     }
